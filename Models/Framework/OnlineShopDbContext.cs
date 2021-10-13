50197a4dd2268f0e3a058de06ae89effd29aec03
Minh Phan Ngoc <phanminh172@gmail.com>
2021-10-14 00:26:42 +0700

    first commit

---
+++ Models/Framework/OnlineShopDbContext.cs
@@ -0,0 +1,27 @@
+namespace Models.Framework
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Linq;
+    public partial class OnlineShopDbContext : DbContext
+    {
+        public OnlineShopDbContext()
+            : base("name=OnlineShopDbContext")
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.UserName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+        }
+    }
+}
--- OnlineShop/Areas/Admin/Controllers/LoginController.cs
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -6,13 +6,12 @@ using System.Web.Mvc;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class LoginController : Controller
     {
-        // GET: Admin/Home
+        // GET: Admin/Login
         public ActionResult Index()
         {
             return View();
         }
-        
     }
 }
